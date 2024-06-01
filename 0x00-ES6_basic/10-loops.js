8396fbd8c677beec1c562305a9ab9107dd1c0b04
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-06-01 11:50:52 +0000

    For.. of loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const modifiedArray = [];
+  for (const value of array) {
+    modifiedArray.push(appendString + value);
+  }
+  return modifiedArray;
+}
