7f7d035ee6feb1b6d3d565e456ebfe78dec65ce2
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-06-19 19:37:28 +0000

    some classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,6 @@
 import ClassRoom from './0-classroom.js';
-function initiliazeRooms() {
-	const room1 = new ClassRoom(19)
-	const room2 = new ClassRoom(20)
-	const room3 = new ClassRoom(34)
-	return [room1, room2, room3];
+function initializeRooms() {
+  const sizes = [19, 20, 34];
+  return sizes.map(size => new ClassRoom(size));
 }
+export default initializeRooms;
