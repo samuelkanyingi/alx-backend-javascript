2810845bf95a876176eaff86aa4085531ea3d10d
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-06-20 05:45:56 +0000

    building class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,22 +1,22 @@
 class Building {
-    constructor(sqft) {
-        // Validate that sqft is a number
-        if (typeof sqft !== 'number') {
-            throw new TypeError('Sqft must be a number');
-        }
-
-        this._sqft = sqft;
-
-        // Check if the subclass has implemented evacuationWarningMessage
-        if (this.constructor !== Building && !this.evacuationWarningMessage) {
-            throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
+  constructor(sqft) {
+    // Validate that sqft is a number
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
     }
 
-    // Getter for sqft
-    get sqft() {
-        return this._sqft;
+    this._sqft = sqft;
+
+    // Check if the subclass has implemented evacuationWarningMessage
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+  }
+
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
 }
 
 export default Building;
