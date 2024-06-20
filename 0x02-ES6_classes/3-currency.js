87d37464ac5ef3210f3b6df19c286c4c200bff14
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-06-20 05:14:44 +0000

    Methods, static methods, computed methods names

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,47 +1,47 @@
 class Currency {
-    constructor(code, name) {
-        // Validate types
-        this._validateString(code, 'Code');
-        this._validateString(name, 'Name');
+  constructor(code, name) {
+    // Validate types
+    this._validateString(code, 'Code');
+    this._validateString(name, 'Name');
 
-        // Assign attributes
-        this._code = code;
-        this._name = name;
-    }
+    // Assign attributes
+    this._code = code;
+    this._name = name;
+  }
 
-    // Getter for code
-    get code() {
-        return this._code;
-    }
+  // Getter for code
+  get code() {
+    return this._code;
+  }
 
-    // Setter for code
-    set code(newCode) {
-        this._validateString(newCode, 'Code');
-        this._code = newCode;
-    }
+  // Setter for code
+  set code(newCode) {
+    this._validateString(newCode, 'Code');
+    this._code = newCode;
+  }
 
-    // Getter for name
-    get name() {
-        return this._name;
-    }
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-    // Setter for name
-    set name(newName) {
-        this._validateString(newName, 'Name');
-        this._name = newName;
-    }
+  // Setter for name
+  set name(newName) {
+    this._validateString(newName, 'Name');
+    this._name = newName;
+  }
 
-    // Method to display the full currency format
-    displayFullCurrency() {
-        return `${this._name} (${this._code})`;
-    }
+  // Method to display the full currency format
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 
-    // Validation method
-    _validateString(value, attributeName) {
-        if (typeof value !== 'string') {
-            throw new TypeError(`${attributeName} must be a string`);
-        }
+  // Validation method
+ static _validateString(value, attributeName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${attributeName} must be a string`);
     }
+  }
 }
 
 export default Currency;
