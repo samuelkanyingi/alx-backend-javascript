28ae6f6632a74b77a3fa91dd3381645c92cc5600
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-07-24 11:18:56 +0000

    Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.trim().split('\n').filter((line) => line.trim() !== '');
+    if (lines.length === 0) {
+      throw new Error('Cannot load database');
+    }
+    const [, ...studentLines] = lines;
+    // const [firstname, lastname, age, field] = header.split(',');
+
+    const studentsByField = {};
+
+    studentLines.forEach((line) => {
+      const [first, , , fld] = line.split(',');
+      if (!studentsByField[fld]) {
+        studentsByField[fld] = [];
+      }
+      studentsByField[fld].push(first);
+    });
+    const totalStudents = studentLines.length;
+    console.log(`Number of students: ${totalStudents}`);
+    Object.keys(studentsByField).forEach((fld) => {
+      const names = studentsByField[fld].join(', ');
+      console.log(`Number of students in ${fld}: ${studentsByField[fld].length}. List: ${names}`);
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+module.exports = countStudents;
