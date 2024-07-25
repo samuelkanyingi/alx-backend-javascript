53947d76c38057b748e63d37011bb82f9ab17d06
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-07-25 11:52:24 +0000

    Combining descriptions

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul.js');
+
+describe('calculateNumber', function() {
+  describe('SUM', function() {
+    it('should return 6 for inputs (1.4, 4.5)', function() {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+  });
+  describe('SUBTRACT', function() {
+    it('should return -4 for inputs (1.4, 4.5)', function() {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+  });
+
+  describe('DIVIDE', function() {
+    it('should return 0.2 for inputs (1.4, 4.5)', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+
+    it('should return "Error" for inputs (1.4, 0)', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+  });
+});
