b17b1f6ad3e468bce81ea2425cdb06d8f3008bb2
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-07-25 04:17:20 +0000

    Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,8 +5,9 @@ const port = 1245;
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
-app.listen(port,() => {
-console.log(`Server running at http://localhost:${port}/`);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
+module.exports = app;
