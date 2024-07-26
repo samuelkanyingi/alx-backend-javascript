23450952f12608d5bde874a4d86b41f1636b4a74
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-07-26 06:21:50 +0000

    Create Spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,22 +1,16 @@
-#!/usr/bin/python3
+// 3-payment.test.js
 const sinon = require('sinon');
 const { expect } = require('chai');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
-describe('sendPaymentRequestToApi', () => {
-  let calculateNumberSpy;
+describe('sendPaymentRequestToApi', function() {
+  it('should call Utils.calculateNumber with SUM, 100, 20', function() {
+    const spy = sinon.spy(Utils, 'calculateNumber');
 
-  beforeEach(() => {
-    calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-  });
-
-  afterEach(() => {
-    calculateNumberSpy.restore();
-  });
-
-  it('should call Utils.calculateNumber with correct arguments', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+
+    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    spy.restore();
   });
 });
