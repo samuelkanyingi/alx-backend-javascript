a76526533a9300d7d01df82a4d3ec473709ebafc
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-07-26 10:37:27 +0000

    Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,17 +2,22 @@
 const request = require('request');
 const { expect } = require('chai');
 let server;
+const port = 7865;
 
 describe('Index page', function () {
   before(function (done) {
+   try {
     // Start the server before tests
     server = require('./api'); // Import the server instance
     // Wait a moment to ensure the server is up and running
     setTimeout(done, 1000);
+  } catch (err) {
+    done(err);
+  }
   });
 
   it('should return status code 200 and the correct message', function (done) {
-    request('http://localhost:7865/', function (error, response, body) {
+    request(`http://localhost:${port}/`, function (error, response, body) {
       if (error) return done(error);
 
       expect(response.statusCode).to.equal(200);
@@ -23,7 +28,7 @@ describe('Index page', function () {
 
   after(function (done) {
     // Close the server after tests
-    if (server) {
+    if (server && typeof server.close === 'function') {
       server.close(done);
     } else {
       done();
