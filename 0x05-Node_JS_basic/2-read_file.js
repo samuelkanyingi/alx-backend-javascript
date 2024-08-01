ac639d47520f7b8c8223afdaa6384555f5ea8e44
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-08-01 06:42:05 +0000

    synchronous

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,17 +2,11 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    if (!fs.existsSync(path)) {
-      throw new Error('Cannot load the database');
-    }
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.split('\n').filter((line) => line.trim() !== '');
     const students = lines.slice(1).filter(line => line.trim() !== '');
-; // Skip header
-
     const NUMBER_OF_STUDENTS = students.length;
     console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
-
     const fields = {};
     students.forEach((student) => {
       const [firstname, lastname, age, field] = student.split(',');
@@ -26,8 +20,8 @@ function countStudents(path) {
       console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
     }
   } catch (error) {
-    throw new Error('Cannot load the database');
-  }
+      throw new Error('Cannot load the database');
+    }
 }
 
 module.exports = countStudents;
