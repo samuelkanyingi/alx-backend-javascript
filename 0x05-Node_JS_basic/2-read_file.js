cf4de926540f487ea6878e0a0f31c340e141b4d6
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-08-01 04:39:06 +0000

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -6,7 +6,7 @@ function countStudents(path) {
     const lines = data.trim().split('\n').filter((line) => line.trim() !== '');
     if (lines.length <= 1) {
       console.log('Number of students: 0');
-	    return;
+      return;
     }
     const [, ...studentLines] = lines;
     // const [firstname, lastname, age, field] = header.split(',');
@@ -27,11 +27,11 @@ function countStudents(path) {
       console.log(`Number of students in ${fld}: ${studentsByField[fld].length}. List: ${names}`);
     });
   } catch (error) {
-	  if (error.code === 'ENOENT') {
-            throw new Error('Cannot load the database');
-        } else {
-            throw error;
-        }
+    if (error.code === 'ENOENT') {
+      throw new Error('Cannot load the database');
+    } else {
+      throw error;
+    }
   }
 }
 module.exports = countStudents;
